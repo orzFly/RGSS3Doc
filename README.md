09dfde0a643ddd914877628a986499d3a6336685
Yeechan Lu <wz.bluesnow@gmail.com>
2013-03-16 14:25:38 +0800

    Initial

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 RGSSDoc
 =======
+
+```sh
+gem install sdoc
+origin=`pwd`
+pushd /path/to/your/gems/sdoc-0.3.20
+patch -p1 < $origin/sdoc.patch
+popd
+
+rake
+```
---
+++ gems/rgss3/dataclass.rb
@@ -0,0 +1,138 @@
+
+module RPG
+end
+
+class RPG::SE
+end
+
+class RPG::ME
+end
+
+class RPG::BGS
+end
+
+class RPG::BGM
+end
+
+class RPG::AudioFile
+end
+
+class RPG::System::TestBattler
+end
+
+class RPG::System::Terms
+end
+
+class RPG::System::Vehicle
+end
+
+class RPG::System
+end
+
+class RPG::CommonEvent
+end
+
+class RPG::Tileset
+end
+
+class RPG::Animation::Timing
+end
+
+class RPG::Animation::Frame
+end
+
+class RPG::Animation
+end
+
+class RPG::Troop::Page::Condition
+end
+
+class RPG::Troop::Page
+end
+
+class RPG::Troop::Member
+end
+
+class RPG::Troop
+end
+
+class RPG::Enemy::Action
+end
+
+class RPG::Enemy::DropItem
+end
+
+class RPG::Class::Learning
+end
+
+class RPG::UsableItem::Effect
+end
+
+class RPG::UsableItem::Damage
+end
+
+class RPG::BaseItem::Feature
+end
+
+class RPG::State
+end
+
+class RPG::Enemy
+end
+
+class RPG::Armor
+end
+
+class RPG::Weapon
+end
+
+class RPG::EquipItem
+end
+
+class RPG::Item
+end
+
+class RPG::Skill
+end
+
+class RPG::UsableItem
+end
+
+class RPG::Class
+end
+
+class RPG::Actor
+end
+
+class RPG::BaseItem
+end
+
+class RPG::MoveCommand
+end
+
+class RPG::MoveRoute
+end
+
+class RPG::EventCommand
+end
+
+class RPG::Event::Page::Graphic
+end
+
+class RPG::Event::Page::Condition
+end
+
+class RPG::Event::Page
+end
+
+class RPG::Event
+end
+
+class RPG::MapInfo
+end
+
+class RPG::Map::Encounter
+end
+
+class RPG::Map
+end
